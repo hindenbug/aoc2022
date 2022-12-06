86a1888c30a1f552d465a3207c29111b2ad6bb74
hindenbug <304184+hindenbug@users.noreply.github.com>
2022-12-06 22:38:12 +0530

    day1: part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::VecDeque;
+
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("{}", part1(input))
+    println!("{}", part1(input));
+    println!("{}", part2(input));
 }
 
 fn part1(input: &str) -> u32 {
@@ -10,4 +13,15 @@ fn part1(input: &str) -> u32 {
         .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>())
         .max()
         .unwrap();
+}
+
+fn part2(input: &str) -> u32 {
+    let mut calories = input
+        .split("\n\n")
+        .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>())
+        .collect::<Vec<u32>>();
+
+    calories.sort();
+
+    return calories.into_iter().rev().take(3).sum::<u32>();
 }
