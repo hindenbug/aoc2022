cec13fbbeddcf7d3bc0c8df49ec9d922e8b69c64
hindenbug <304184+hindenbug@users.noreply.github.com>
2022-12-20 18:28:20 +0530

    day2: part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -38,7 +38,7 @@ impl Outcome {
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("{:?}", part1(input))
+    println!("{:?}, {:?}", part1(input), part2(input))
 }
 
 fn part1(input: &str) -> u32 {
@@ -51,23 +51,63 @@ fn part1(input: &str) -> u32 {
         }.unwrap())
        .collect();
 
-    return moves.iter()
+   return calculate_score(moves)
+}
+
+fn part2(input: &str) -> u32 {
+    let moves: Vec<(Move, Move)> = input
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|line| {
+            line.split_once(' ')
+                .map(|s| {
+                    match s.1 {
+                        "X" => {
+                            let move_ = match Move::from_str(s.0) {
+                                Move::Rock => Move::Scissors,
+                                Move::Paper => Move::Rock,
+                                Move::Scissors => Move::Paper,
+                            };
+
+                            (Move::from_str(s.0), move_)
+                        }
+                        "Y" => (Move::from_str(s.0), Move::from_str(s.0)),
+                        "Z" => {
+                          let move_ = match Move::from_str(s.0) {
+                                Move::Rock => Move::Paper,
+                                Move::Paper => Move::Scissors,
+                                Move::Scissors => Move::Rock,
+                            };
+                            (Move::from_str(s.0), move_)
+                        }
+                        _ => unreachable!()
+                    }
+                })
+        }.unwrap())
+        .collect();
+
+    return calculate_score(moves)
+}
+
+fn calculate_score(moves: Vec<(Move, Move)>) -> u32 {
+    moves.iter()
         .map(|(l, r)| {
-           let score: u32 = match (l, r) {
-            (Move::Rock, Move::Scissors) | (Move::Paper, Move::Rock) | (Move::Scissors, Move::Paper)  => Outcome::value(&Outcome::Lose),
-            (Move::Paper, Move::Scissors) | (Move::Scissors, Move::Rock) | (Move::Rock, Move::Paper) => Outcome::value(&Outcome::Win),
-            _ => Outcome::value(&Outcome::Tie),
-        };
+            let score: u32 = match (l, r) {
+                (Move::Rock, Move::Scissors) | (Move::Paper, Move::Rock) | (Move::Scissors, Move::Paper)  => Outcome::value(&Outcome::Lose),
+                (Move::Paper, Move::Scissors) | (Move::Scissors, Move::Rock) | (Move::Rock, Move::Paper) => Outcome::value(&Outcome::Win),
+                _ => Outcome::value(&Outcome::Tie),
+            };
 
             score + *r as u32 + 1
-    })
-        .sum()
+        }).sum()
 }
+
 
 
 #[cfg(test)]
 mod tests {
     use crate::part1;
+    use crate::part2;
 
     const INPUT: &str = r#"
 A Y
@@ -78,5 +118,10 @@ C Z
     #[test]
     fn test_part1() {
         assert_eq!(15, part1(INPUT));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(12, part2(INPUT));
     }
 }
