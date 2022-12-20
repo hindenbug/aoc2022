c62ad9a861513409ce32ac9feabe3b33555f6e6f
hindenbug <304184+hindenbug@users.noreply.github.com>
2022-12-20 21:04:46 +0530

    day3: part 1

---
+++ day03/src/main.rs
@@ -0,0 +1,47 @@
+use array_tool::vec::Intersect;
+
+
+const CHARS: &str= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("{:?}", part1(input))
+}
+
+fn part1(input: &str) -> u32 {
+  let sum: u32 = input
+       .lines()
+       .map(|line| {
+           let (first, last) = line.split_at(line.len() / 2);
+           let p1 = first.chars().collect::<Vec<_>>();
+           let p2 = last.chars().collect::<Vec<_>>();
+           p1.intersect(p2)
+       })
+      .flatten()
+      .map(|c| {
+         CHARS.chars().into_iter().position(|r| r == c).unwrap() as u32 + 1 as u32
+      })
+      .sum();
+
+    return sum
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::part1;
+
+    const INPUT: &str = r#"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"#;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(157, part1(INPUT));
+    }
+}
