80b366a0550837f87f8b152b6778ce8e4e2f967a
hindenbug <304184+hindenbug@users.noreply.github.com>
2022-12-20 21:44:13 +0530

    day3: part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -6,7 +6,7 @@ const CHARS: &str= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("{:?}", part1(input))
+    println!("{:?}, {:?}", part1(input), part2(input))
 }
 
 fn part1(input: &str) -> u32 {
@@ -27,10 +27,31 @@ fn part1(input: &str) -> u32 {
     return sum
 }
 
+fn part2(input: &str) -> u32 {
+    let sum: u32  = input
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks_exact(3)
+        .map(|group| {
+            let p1 = group[0].chars().collect::<Vec<_>>();
+            let p2 = group[1].chars().collect::<Vec<_>>();
+            let p3 = group[2].chars().collect::<Vec<_>>();
+            p1.intersect(p2).intersect(p3)
+        })
+        .flatten()
+        .map(|c| {
+            CHARS.chars().into_iter().position(|r| r == c).unwrap() as u32 + 1 as u32
+        })
+        .sum();
+
+    return sum
+}
+
 
 #[cfg(test)]
 mod tests {
     use crate::part1;
+    use crate::part2;
 
     const INPUT: &str = r#"vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -43,5 +64,10 @@ CrZsJsPPZsGzwwsLwLmpwMDw
     #[test]
     fn test_part1() {
         assert_eq!(157, part1(INPUT));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(70, part2(INPUT));
     }
 }
