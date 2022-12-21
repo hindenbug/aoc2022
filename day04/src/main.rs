7ddc26299d468fba347b9120104f279d438482e9
hindenbug <304184+hindenbug@users.noreply.github.com>
2022-12-21 13:54:41 +0530

    day4: part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("{:?}, {:?}", part1(input), "")
+    println!("{:?}, {:?}", part1(input), part2(input))
 }
 
 fn part1(input: &str) -> u32  {
@@ -17,6 +17,19 @@ fn part1(input: &str) -> u32  {
        .count() as u32
 }
 
+fn part2(input: &str) -> u32 {
+    return input
+        .lines()
+        .map(|line| {
+            let (left, right) = line.split_once(',').unwrap();
+            (to_pair(left), to_pair(right))
+        })
+        .filter(|(l, r)| {
+            l.0 <= r.1 && r.0 <= l.1
+        })
+        .count() as u32
+}
+
 
 fn to_pair(input: &str) -> (u32, u32) {
     let (left, right) = input.split_once('-').unwrap();
@@ -26,6 +39,7 @@ fn to_pair(input: &str) -> (u32, u32) {
 #[cfg(test)]
 mod tests {
     use crate::part1;
+    use crate::part2;
 
     const INPUT: &str = r#"2-4,6-8
 2-3,4-5
@@ -42,5 +56,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
+        assert_eq!(4, part2(INPUT));
     }
 }
